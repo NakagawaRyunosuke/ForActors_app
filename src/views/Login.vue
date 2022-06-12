abe00bf16f656babe476cded1117b134af3ea228
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-06-12 21:46:32 +0900

    edit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -37,7 +37,7 @@
 </template>
 
 <script>
-import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, getRedirectResult } from "firebase/auth";
 import db from "../plugins/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -46,7 +46,7 @@ export default {
         googleAuth(){
             const provider = new GoogleAuthProvider();
             const auth = getAuth();
-            signInWithRedirect(auth,provider)
+            getRedirectResult(auth,provider)
             .then((result)=>{
                 console.log(result.user);
                 this.$store.state.uid = result.user.uid;
