eed5f339c2a786f264ffe0421d6f013d131c579f
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-06-12 22:39:59 +0900

    edit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -79,12 +79,11 @@ export default {
         const auth = getAuth();
         getRedirectResult(auth)
         .then((result)=>{
-            console.log(result.user);
             this.$store.state.uid = result.user.uid;
             sessionStorage.setItem('user', this.$store.state.uid);
             this.checkData();
             this.$router.push("/");
-            this.$router.go({path: this.$router.currentRoute.path, force: true});
+            //this.$router.go({path: this.$router.currentRoute.path, force: true});
         })
         .catch((error)=>{
             const errorCode = error.code;
